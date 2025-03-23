0a45637c279e8f4bb35303b75ce484afbf00e367
turik777 <52492191+turik777@users.noreply.github.com>
2025-03-24 01:32:07 +0300

    feat: complete getNextFriday function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -61,8 +61,12 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const dateNew = new Date(date);
+  const daysToFriday =
+    dateNew.getDay() === 5 ? 7 : (7 + 5 - dateNew.getDay()) % 7;
+  dateNew.setDate(dateNew.getDate() + daysToFriday);
+  return dateNew;
 }
 
 /**
