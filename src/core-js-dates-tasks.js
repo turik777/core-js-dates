16ecb4c9823a585132a982d690448644af0dc612
turik777 <52492191+turik777@users.noreply.github.com>
2025-03-24 04:32:42 +0300

    feat: complete getNextFridayThe13th function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -205,8 +205,19 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let date13 = new Date(year, month, 13);
+  while (date13.getDay() !== 5) {
+    month += 1;
+    if (month > 11) {
+      month = 0;
+      year += 1;
+    }
+    date13 = new Date(year, month, 13);
+  }
+  return date13;
 }
 
 /**
